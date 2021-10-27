41cb3a0981615f8ca529f37ee8cf1e31d33b0ca3
Chi Nguyen <chinsu190103@gmail.com>
2021-10-26 21:30:54 -0700

    Change in three pages

--- scripts/firebaseAPI.js
+++ scripts/firebaseAPI.js
@@ -1,13 +1,13 @@
 // Your web app's Firebase configuration
-var  firebaseConfig = {
-    apiKey: ,
-    authDomain: ",
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId: 
-  };
-  
+var firebaseConfig = {
+  apiKey: "AIzaSyAj9T116YIVa7-xfrFhSZfwlkcPaxVfooo",
+  authDomain: "demo08-chin.firebaseapp.com",
+  projectId: "demo08-chin",
+  storageBucket: "demo08-chin.appspot.com",
+  messagingSenderId: "292518547833",
+  appId: "1:292518547833:web:709362d70a54c2ff4d5fa8"
+};
 
-  const app = firebase.initializeApp(firebaseConfig);
-  
+
+const app = firebase.initializeApp(firebaseConfig);
+
